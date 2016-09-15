4354d3877c65839b93b5db806d1894026ff60299
Ivan Zhang <ivanzhangemail@gmail.com>
2016-09-15 19:17:10 -0400

    Added Additional TextFields

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    // Weak = Weak reference. Our super view owns a reference to this PickerView. In the code also keeps a reference here. If the screen is gone, then both the references are released. 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska", "Arkansas", "Alabama", "Maine", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self // When changes happen to the state picker, look to this .swift
+        statePicker.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     
     }
@@ -20,7 +30,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func stateBtnPressed(sender: AnyObject) {
+        statePicker.hidden = false
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], forState: UIControlState.Normal)
+        statePicker.hidden = true
 
+    }
 
 }
 
